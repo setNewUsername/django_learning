a3da634c23b25249f4504df2444230077cc388b3
andrew <sanakurin@gmail.com>
2022-09-26 15:52:48 +0300

    added logo

--- stock/models.py
+++ stock/models.py
@@ -7,9 +7,13 @@ class Stock(models.Model):
     ticker = models.CharField(max_length=4, default="NULL")
     description = models.TextField(null=True, blank=True)
     currency = models.ForeignKey('Currency', null=True, on_delete=models.SET_NULL)
-    
+    logo = models.ImageField(null=True, blank=True)
+
     def get_random_price(self):
         return random.randint(0, 3000)
+
+    def __str__(self):
+        return f"{self.ticker}"
 
 class Currency(models.Model):
     name = models.CharField(max_length=40)
